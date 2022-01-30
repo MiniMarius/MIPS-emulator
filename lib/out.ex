6f9ca4bd9f3fb213f60a04ff9a4e18ddf44ff2b6
MiniMarius <seffermarius@gmail.com>
2022-01-30 16:32:25 +0100

    output is now more fancy

--- lib/out.ex
+++ lib/out.ex
@@ -2,8 +2,9 @@ defmodule Out do
 
   def new() do  [] end
 
+  #
   def put(out, a) do [a|out] end
 
-  def close(out) do Enum.reverse(out) end
+  def close(list) do IO.puts("list of integers is: [#{Enum.join(list, ", ")}]") end
 
 end
